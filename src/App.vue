cfee5525e8c6e0c38eff857fb679796ed470848d
Mattan41 <Mats.kruskopf.eriksson@iths.se>
2024-09-20 17:08:36 +0200

    update AppHeader

--- src/App.vue
+++ src/App.vue
@@ -4,8 +4,7 @@ import TheCombatList from './components/TheCombatList.vue'
 import AppHeader from './components/AppHeader.vue'
 import {ref} from "vue";
 
-
-const showMonsterList = ref(true)
+const showMonsterList = ref(false)
 
 const toggleMonsterList = () => {
   showMonsterList.value = !showMonsterList.value
@@ -13,9 +12,9 @@ const toggleMonsterList = () => {
 </script>
 
 <template>
-  <AppHeader @toggleMonsterList="toggleMonsterList">
-    {{ showMonsterList ? 'Hide' : 'Show' }} Monster List
-  </AppHeader>
+  <header>
+    <AppHeader :showMonsterList="showMonsterList" @toggleMonsterList="toggleMonsterList"/>
+  </header>
   <main :class="{ 'single-column': !showMonsterList }">
     <TheMonsterList v-show="showMonsterList" addMonsterButton="add monster"/>
     <TheCombatList/>
@@ -35,6 +34,7 @@ main {
     grid-template-columns: 1fr 1fr;
     gap: 1rem;
   }
+
   .single-column {
     grid-template-columns: 1fr;
   }
--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,19 +1,22 @@
 <script setup>
-import {defineEmits, computed, ref} from 'vue'
-defineEmits(['toggleMonsterList']);
 
-const showMonsterList = ref(true);
+defineProps({
+  showMonsterList: {
+    type: Boolean,
+    required: true
+  }
+})
 
-const buttonText = computed(() => {
-  return showMonsterList.value ? 'Hide Monster List' : 'Show Monster List';
-});
+const emit = defineEmits(['toggleMonsterList'])
+
+
 </script>
 
 <template>
   <div class="app-header">
     <h1 class="app-title">5e Encounter Builder</h1>
     <button @click="$emit('toggleMonsterList')">
-      <span class="button-text">{{ buttonText }}</span>
+      <span class="button-text">{{ showMonsterList ? 'Hide Monster List' : 'Show Monster List' }}</span>
       <span class="button-text-mobile">Monster List</span>
     </button>
   </div>
@@ -64,6 +67,7 @@ button:hover {
   .button-text {
     display: inline;
   }
+
   .button-text-mobile {
     display: none;
   }
